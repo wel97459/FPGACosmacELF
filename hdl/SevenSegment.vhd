e13bef365bfd6a93718a17090d4d4c44590d3cf0
Winston Lowe <wel97459@gmail.com>
2020-01-14 19:02:36 -0800

    Update SevenSegment.vhd

--- hdl/SevenSegment.vhd
+++ hdl/SevenSegment.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company:
--- Engineer:
---
--- Create Date:    21:43:48 12/22/2017
--- Design Name:
--- Module Name:    SevenSegment - Behavioral
--- Project Name:
--- Target Devices:
--- Tool versions:
--- Description:
---
--- Dependencies:
---
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
